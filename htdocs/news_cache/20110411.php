4b7ad67b452a9c222f4da45de9f70b94782741d6
Andrei Nicholson <andrei@gamesnapper.com>
2011-06-09 19:42:22 -0600

    Add news page

---
+++ htdocs/news_cache/20110411.php
@@ -0,0 +1,24 @@
+<?php
+
+$title = 'Launch!';
+
+$posted = mktime(0, 0, 0, 4, 11, 2011);
+
+$post = <<<HTML
+<p>Finally launched the initial version of GameSnapper on 2011-04-10. First
+started in late November of 2010 and created little by little over that
+period, opening with 288 games and little features.</p>
+
+<p>The initial list of planned features was quite intense looking over it in
+hindsight but many things had to be cut. Why cut features visitors may
+like?</p>
+
+<p>It's better to launch early omitting features not known to be useful and
+listen to the needs of the visitors, adding features and omitting those that
+were in the initial release. Launch early and iterate often!</p>
+
+<p>So my first and foremost goal at this stage is to pay attention to what
+people want, what they most often use, what they use least, and change
+GameSnapper accordingly. I ask everyone who visits to drop me a line on the
+good and the bad of their visit.</p>
+HTML;
