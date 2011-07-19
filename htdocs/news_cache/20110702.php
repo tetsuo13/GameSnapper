03165938628decf4a39ed00321c6aa801e681ec4
Andrei Nicholson <andrei@gamesnapper.com>
2011-07-18 18:28:41 -0600

    Fix up grammar

--- htdocs/news_cache/20110702.php
+++ htdocs/news_cache/20110702.php
@@ -7,9 +7,9 @@ $posted = mktime(0, 0, 0, 7, 2, 2011);
 $post = <<<HTML
 <p>One of my all-time biggest pet peeves when visiting other Flash gaming
 sites on my laptop is how small the game is compared to everything else on the
-screen. It's almost makes me squint my eyes.</p>
+screen. It almost makes me squint my eyes.</p>
 
 <p>This is why there is the full screen mode now on all games. Go to any game
-and directly underneat in to the right is a link to "Full Screen Mode." This
-will black everything else out and maximize the game to fit your screen.</p>
+and directly underneath in to the right is a link to "Full Screen Mode." This
+will darken everything else out and maximize the game to fit your screen.</p>
 HTML;
